9a1cda79c76fc1d51904eddee935b8c2b3a6e0c6
Sascha Goldhofer <post@saschagoldhofer.de>
2017-11-11 13:02:19 +0100

    Update iterateSchema to iterate most subSchemas

--- test/unit/iterateSchema.test.js
+++ test/unit/iterateSchema.test.js
@@ -106,7 +106,39 @@ describe("iterateSchema", () => {
         expect(calls[2]).to.eq(rootSchema.items.oneOf[1]);
     });
 
-    it("should call on defintitions", () => {
+    it("should call on each anyOf-schema", () => {
+        const calls = [];
+        const rootSchema = {
+            anyOf: [
+                { type: "string" },
+                { type: "number" }
+            ]
+        };
+
+        iterateSchema(rootSchema, (schema) => calls.push(schema));
+
+        expect(calls).to.have.length(3);
+        expect(calls[1]).to.eq(rootSchema.anyOf[0]);
+        expect(calls[2]).to.eq(rootSchema.anyOf[1]);
+    });
+
+    it("should call on each allOf-schema", () => {
+        const calls = [];
+        const rootSchema = {
+            allOf: [
+                { type: "string" },
+                { type: "number" }
+            ]
+        };
+
+        iterateSchema(rootSchema, (schema) => calls.push(schema));
+
+        expect(calls).to.have.length(3);
+        expect(calls[1]).to.eq(rootSchema.allOf[0]);
+        expect(calls[2]).to.eq(rootSchema.allOf[1]);
+    });
+
+    it("should call on definitions", () => {
         const calls = [];
         const rootSchema = {
             definitions: {
@@ -120,5 +152,52 @@ describe("iterateSchema", () => {
 
         expect(calls).to.have.length(2);
         expect(calls[1]).to.eq(rootSchema.definitions.image);
+    });
+
+    it("should call on additionalProperties", () => {
+        const calls = [];
+        const rootSchema = {
+            additionalProperties: {
+                type: "object",
+                properties: {
+                    url: { type: "string" }
+                }
+            }
+        };
+
+        iterateSchema(rootSchema, (schema) => calls.push(schema));
+
+        expect(calls).to.have.length(3);
+        expect(calls[1]).to.eq(rootSchema.additionalProperties);
+    });
+
+    it("should ignore depedency list", () => {
+        const calls = [];
+        const rootSchema = {
+            dependencies: {
+                url: ["title"]
+            }
+        };
+
+        iterateSchema(rootSchema, (schema) => calls.push(schema));
+
+        expect(calls).to.have.length(1);
+    });
+
+    it("should call on each depedency schema", () => {
+        const calls = [];
+        const rootSchema = {
+            dependencies: {
+                url: ["title"],
+                target: {
+                    type: "string"
+                }
+            }
+        };
+
+        iterateSchema(rootSchema, (schema) => calls.push(schema));
+
+        expect(calls).to.have.length(2);
+        expect(calls[1]).to.eq(rootSchema.dependencies.target);
     });
 });
