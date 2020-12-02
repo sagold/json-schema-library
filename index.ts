8e5f0313645ff3a007518f902b6febf72e0bdb21
Sascha Goldhofer <sascha.goldhofer@sueddeutsche.de>
2020-12-02 09:44:15 +0100

    Convert index to use es6 modiules

--- index.ts
+++ index.ts
@@ -1,29 +1,48 @@
 import strings from "./lib/config/strings";
+import Interface from "./lib/cores/CoreInterface";
+import Draft04 from "./lib/cores/Draft04";
+import JsonEditor from "./lib/cores/JsonEditor";
+import addSchema from "./lib/addSchema";
+import addValidator from "./lib/addValidator";
+import compileSchema from "./lib/compileSchema";
+import createCustomError from "./lib/utils/createCustomError";
+import createSchemaOf from "./lib/createSchemaOf";
+import each from "./lib/each";
+import eachSchema from "./lib/eachSchema";
+import getChildSchemaSelection from "./lib/getChildSchemaSelection";
+import getSchema from "./lib/getSchema";
+import getTemplate from "./lib/getTemplate";
+import getTypeOf from "./lib/getTypeOf";
+import isValid from "./lib/isValid";
+import SchemaService from "./lib/SchemaService";
+import step from "./lib/step";
+import validate from "./lib/validate";
+import validateAsync from "./lib/validateAsync";
 
 
-module.exports = {
+export default {
     config: {
         strings
     },
     cores: {
-        Interface: require("./lib/cores/CoreInterface"),
-        Draft04: require("./lib/cores/Draft04"), // core implementing draft04 specs
-        JsonEditor: require("./lib/cores/JsonEditor") // adjusted core of draft04 to better support the json-editor
+        Interface,
+        Draft04, // core implementing draft04 specs
+        JsonEditor // adjusted core of draft04 to better support the json-editor
     },
-    addSchema: require("./lib/addSchema"), // add a schema to be references via $ref
-    addValidator: require("./lib/addValidator"), // add validation for keyword, format, datatype and customize errors
-    compileSchema: require("./lib/compileSchema"),
-    createCustomError: require("./lib/utils/createCustomError"),
-    createSchemaOf: require("./lib/createSchemaOf"), // creates a simple schema based on the given data
-    each: require("./lib/each"), // iterate over data, receiving each data-entry with its schema
-    eachSchema: require("./lib/eachSchema"), // iterates over a json-schemas type definitions
-    getChildSchemaSelection: require("./lib/getChildSchemaSelection"), // get available child schemas
-    getSchema: require("./lib/getSchema"), // get schema of datapointer
-    getTemplate: require("./lib/getTemplate"), // create data which is valid to the given schema
-    getTypeOf: require("./lib/getTypeOf"), // returns the javascript datatype
-    isValid: require("./lib/isValid"), // returns a boolean if the schema is valid
-    SchemaService: require("./lib/SchemaService"),
-    step: require("./lib/step"), // steps into a json-schema, returning the matching child-schema
-    validate: require("./lib/validate"), // validates data by a schema
-    validateAsync: require("./lib/validateAsync") // async validation of data by a schema
+    addSchema, // add a schema to be references via $ref
+    addValidator, // add validation for keyword, format, datatype and customize errors
+    compileSchema,
+    createCustomError,
+    createSchemaOf, // creates a simple schema based on the given data
+    each, // iterate over data, receiving each data-entry with its schema
+    eachSchema, // iterates over a json-schemas type definitions
+    getChildSchemaSelection, // get available child schemas
+    getSchema, // get schema of datapointer
+    getTemplate, // create data which is valid to the given schema
+    getTypeOf, // returns the javascript datatype
+    isValid, // returns a boolean if the schema is valid
+    SchemaService,
+    step, // steps into a json-schema, returning the matching child-schema
+    validate, // validates data by a schema
+    validateAsync // async validation of data by a schema
 };
