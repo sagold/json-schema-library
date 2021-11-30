bf28a1f8ae44e94c6c380581cc5d569269523b6f
Sascha Goldhofer <sascha.goldhofer@asset-metrix.com>
2021-11-30 13:53:19 +0100

    fix: remove allof validation as this breaks get template and is not used in any other case

--- lib/resolveAllOf.ts
+++ lib/resolveAllOf.ts
@@ -4,14 +4,18 @@ import errors from "./validation/errors";
 import { JSONSchema, JSONPointer, JSONError } from "./types";
 import Core from "./cores/CoreInterface";
 
-
-export default function resolveAllOf(core: Core, data: any, schema: JSONSchema = core.rootSchema, pointer: JSONPointer = "#"): JSONSchema|JSONError {
+export default function resolveAllOf(
+    core: Core,
+    data: any,
+    schema: JSONSchema = core.rootSchema,
+    pointer: JSONPointer = "#"
+): JSONSchema | JSONError {
     let mergedSchema = copy(schema);
     for (let i = 0; i < schema.allOf.length; i += 1) {
         const allOfSchema = core.resolveRef(schema.allOf[i]);
-        if (core.isValid(data, allOfSchema, pointer) === false) {
-            return errors.allOfError({ value: data, pointer, allOf: JSON.stringify(schema.allOf) });
-        }
+        // if (core.isValid(data, allOfSchema, pointer) === false) {
+        //     return errors.allOfError({ value: data, pointer, allOf: JSON.stringify(schema.allOf) });
+        // }
         mergedSchema = merge(mergedSchema, allOfSchema);
     }
 
