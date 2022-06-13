643cd55ac36326e968172ccc1d9f819d812b9ff7
Sascha Goldhofer <post@saschagoldhofer.de>
2022-06-13 13:08:02 +0200

    docs: update

--- lib/cores/CoreInterface.ts
+++ lib/cores/CoreInterface.ts
@@ -77,6 +77,18 @@ export default class CoreInterface {
         this.rootSchema = schema;
     }
 
+    /**
+     * Returns the json-schema of the given object property or array item.
+     * e.g. it steps by one key into the data
+     * This helper determines the location of the property within the schema
+     * (additional properties, oneOf, ...) and returns the correct schema.
+     *
+     * @param key    property-name or array-index
+     * @param schema json schema of current data
+     * @param data   parent object or array of key
+     * @param [pointer] json pointer of parent object or array
+     * @return schema or error if failed resolving key
+     */
     step(key: string|number, schema: JSONSchema, data: any, pointer: JSONPointer = "#"): JSONSchema {
         throw new Error("function 'step' is not implemented");
     }
