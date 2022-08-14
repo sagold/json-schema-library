00013c62e32857242fdbe599dad1b030e34afacc
Sascha Goldhofer <sascha.goldhofer@asset-metrix.com>
2022-08-15 01:07:39 +0200

    docs: add merge behaviour in comment

--- lib/utils/merge.ts
+++ lib/utils/merge.ts
@@ -1,3 +1,7 @@
 import deepmerge from "deepmerge";
 const overwriteMerge = (destinationArray, sourceArray) => sourceArray;
+
+/**
+ * returns a new json-schema, where properties are combined and arrays are replaced
+ */
 export default <T, K>(a: T, b: K): T & K => deepmerge(a, b, { arrayMerge: overwriteMerge });
