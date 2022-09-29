ba487b365d0108d4a566cea12aa4ddc2620ea780
Sascha Goldhofer <github@saschagoldhofer.de>
2022-09-29 18:28:23 +0200

    fix: JSONError type

--- lib/types.ts
+++ lib/types.ts
@@ -1,23 +1,28 @@
 import Core from "./cores/CoreInterface";
 
-
-export type JSONSchema = { [p:string]: any };
+export type JSONSchema = { [p: string]: any };
 
 export type JSONPointer = string;
 
 export type JSONError = {
-    type: "error"|"string";
+    type: "error";
     name: string;
     code: string;
     message: string;
     data?: { [p: string]: any };
     [p: string]: any;
-}
+};
 
 export interface JSONValidator {
-    (core: Core, schema: JSONSchema, value: any, pointer: JSONPointer): void|undefined|JSONError|JSONError[];
+    (core: Core, schema: JSONSchema, value: any, pointer: JSONPointer):
+        | void
+        | undefined
+        | JSONError
+        | JSONError[];
 }
 
 export interface JSONTypeValidator {
-    (core: Core, schema: JSONSchema, value: any, pointer: JSONPointer): Array<void|undefined|JSONError|JSONError[]>;
+    (core: Core, schema: JSONSchema, value: any, pointer: JSONPointer): Array<
+        void | undefined | JSONError | JSONError[]
+    >;
 }
