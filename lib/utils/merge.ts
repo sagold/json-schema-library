23b6ade1558d1f305a539fbbe905ed0f942c2a69
Sascha Goldhofer <github@saschagoldhofer.de>
2022-12-04 16:39:29 +0100

    types: fix merge

--- lib/utils/merge.ts
+++ lib/utils/merge.ts
@@ -5,4 +5,5 @@ const overwriteMerge = (destinationArray, sourceArray) => sourceArray;
 /**
  * returns a new json-schema, where properties are combined and arrays are replaced
  */
-export default <T, K>(a: T, b: K): T & K => deepmerge(a, b, { arrayMerge: overwriteMerge });
+export default <T>(a: Partial<T>, b: Partial<T>): T =>
+    deepmerge(a, b, { arrayMerge: overwriteMerge });
--- test/unit/utils/merge.test.ts
+++ test/unit/utils/merge.test.ts
@@ -1,11 +1,9 @@
 import { expect } from "chai";
 import merge from "../../../lib/utils/merge";
 
-
 describe("merge", () => {
-
     it("should merge objects", () => {
-        const res = merge({ a: 1 }, { b: 2 });
+        const res = merge<{ a: number; b: number }>({ a: 1 }, { b: 2 });
 
         expect(res).to.deep.equal({ a: 1, b: 2 });
     });
@@ -20,7 +18,7 @@ describe("merge", () => {
         const a = { a: 1 };
         const b = { b: 2 };
 
-        merge(a, b);
+        merge<{ a: number; b: number }>(a, b);
 
         expect(a).to.deep.equal({ a: 1 });
         expect(b).to.deep.equal({ b: 2 });
