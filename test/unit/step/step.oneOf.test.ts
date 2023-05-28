843e44e3f21ec7f910fa1d628d9c10d033fb21da
Sascha Goldhofer <post@saschagoldhofer.de>
2023-05-28 12:38:39 +0200

    test: oneOf losing remote references

--- test/unit/step/step.oneOf.test.ts
+++ test/unit/step/step.oneOf.test.ts
@@ -53,4 +53,37 @@ describe("step.oneof", () => {
         expect(typeof res.getOneOfOrigin).to.eq("function");
         expect(res.getOneOfOrigin().index).to.eq(1);
     });
+
+    // PR #35 https://github.com/sagold/json-schema-library/pull/35/commits/8b6477113bdfce522081473bb0dd8fd6fe680391
+    it("should maintain references from a remote schema when resolving oneOf with $ref", () => {
+        draft.addRemoteSchema("https://my-other-schema.com/schema.json", {
+            type: "object",
+            properties: {
+                innerTitle: { $ref: "#/definitions/number" }
+            },
+            definitions: {
+                number: { type: "number", title: "Zahl" }
+            }
+        });
+        const schema = draft.compileSchema({
+            type: "object",
+            properties: {
+                title: {
+                    oneOf: [
+                        {
+                            type: "object",
+                            properties: { innerTitle: { type: "string", title: "Zeichenkette" } }
+                        },
+                        { $ref: "https://my-other-schema.com/schema.json" }
+                    ]
+                }
+            }
+        });
+        const res = step(draft, "title", schema, { title: { innerTitle: 111 } });
+
+        expect(res.type).to.eq("object");
+
+        const nextRes = step(draft, "innerTitle", res, { innerTitle: 111 });
+        expect(nextRes.type).to.eq("number");
+    });
 });
