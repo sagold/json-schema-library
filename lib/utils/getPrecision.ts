e3d34c9d7b0126ed193ef730759422869619a57d
Sascha Goldhofer <github@saschagoldhofer.de>
2024-04-06 14:45:52 +0200

    add: support exponents in getPrecision

--- lib/utils/getPrecision.ts
+++ lib/utils/getPrecision.ts
@@ -3,6 +3,9 @@
  */
 export function getPrecision(value: number): number {
     const string = `${value}`;
+    if (string.includes("e-")) {
+        return parseInt(string.replace(/.*e-/, ""));
+    }
     const index = string.indexOf(".");
     return index === -1 ? 0 : string.length - (index + 1);
 }
