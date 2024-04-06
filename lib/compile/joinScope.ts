9f489878f4dde51135eb21c68d50b29e30908205
Sascha Goldhofer <github@saschagoldhofer.de>
2024-04-06 13:01:56 +0200

    fix: join scope with absolute path

--- lib/compile/joinScope.ts
+++ lib/compile/joinScope.ts
@@ -1,6 +1,7 @@
 /* eslint max-statements-per-line: ["error", { "max": 2 }] */
 const suffixes = /(#)+$/;
 const trailingHash = /#$/;
+const startingHashAndSlash = /^[#/]+/;
 const isDomain = /^[^:]+:\/\/[^/]+\//;
 const trailingFragments = /\/[^/]*$/;
 const idAndPointer = /#.*$/;
@@ -12,5 +13,12 @@ export default function joinScope(previous?: string, id?: string) {
     if (previous == null || previous === "" || previous === "#") { return id.replace(trailingHash, ""); }
     if (id[0] === "#") { return `${previous.replace(idAndPointer, "")}${id.replace(suffixes, "")}`; }
     if (isDomain.test(id)) { return id.replace(trailingHash, ""); }
-    return `${previous.replace(trailingFragments, "")}/${id.replace(trailingHash, "")}`;
+
+    if (isDomain.test(previous) && id.startsWith("/")) {
+        // we have a domain that should be joined with an absolute path
+        // thus we have to remove all paths from domain before joining
+        return `${previous.replace(/(^[^:]+:\/\/[^/]+)(.*)/, "$1")}/${id.replace(startingHashAndSlash, "")}`;
+    }
+
+    return `${previous.replace(trailingFragments, "")}/${id.replace(startingHashAndSlash, "")}`;
 }
