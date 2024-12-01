c26625ec946459bb47482a79a8bd0dccf993dede
Sascha Goldhofer <github@saschagoldhofer.de>
2024-12-01 23:48:34 +0100

    fix: integration tests

--- test/integration/validate.Draft04.test.ts
+++ test/integration/validate.Draft04.test.ts
@@ -1,8 +1,9 @@
 import path from "path";
 import fs from "fs";
+import { strict as assert } from "assert";
 import { expect } from "chai";
 import { Draft04 as Core } from "../../lib/draft04";
-import { JsonSchema } from "../../lib/types";
+import { JsonSchema, isJsonError } from "../../lib/types";
 
 describe("validate.Draft04", () => {
     let schema: JsonSchema;
@@ -28,38 +29,52 @@ describe("validate.Draft04", () => {
     describe("resolveRef", () => {
         it("should discard any properties where $ref is used", () => {
             draft.rootSchema = { definitions: { def: { type: "string" } } };
-            const result = draft.resolveRef({ $ref: "#/definitions/def", title: "a definition" });
-            // remove internatls (remoteRef precompilation)
-            delete result.__compiled;
-            delete result.id;
-            expect(result).to.deep.equal({ type: "string" });
+
+            const result = draft.resolveRef(
+                draft.createNode({ $ref: "#/definitions/def", title: "a definition" })
+            );
+
+            expect(result.schema).to.deep.equal({ type: "string" });
         });
     });
 
     describe("oneOf", () => {
         it("should return a matching schema", () => {
-            const result = draft.resolveOneOf(5, {
-                oneOf: [{ type: "string" }, { type: "number" }]
-            });
-            expect(result).to.deep.equal({ type: "number" });
+            const result = draft.resolveOneOf(
+                draft.createNode({
+                    oneOf: [{ type: "string" }, { type: "number" }]
+                }),
+                5
+            );
+            expect(result.schema).to.deep.equal({ __oneOfIndex: 1, type: "number" });
         });
 
         it("should return an error if a matching schema could not be found", () => {
-            const result = draft.resolveOneOf([], {
-                oneOf: [{ type: "string" }, { type: "number" }]
-            });
+            const result = draft.resolveOneOf(
+                draft.createNode({
+                    oneOf: [{ type: "string" }, { type: "number" }]
+                }),
+                []
+            );
+
+            assert(isJsonError(result));
             expect(result.type).to.eq("error");
             expect(result.name).to.eq("OneOfError");
         });
 
         it("should return an error if multiple schemas match the data", () => {
-            const result = draft.resolveOneOf(4, {
-                oneOf: [
-                    { type: "string" },
-                    { type: "number", minimum: 2 },
-                    { type: "number", minimum: 3 }
-                ]
-            });
+            const result = draft.resolveOneOf(
+                draft.createNode({
+                    oneOf: [
+                        { type: "string" },
+                        { type: "number", minimum: 2 },
+                        { type: "number", minimum: 3 }
+                    ]
+                }),
+                4
+            );
+
+            assert(isJsonError(result));
             expect(result.type).to.eq("error");
             expect(result.name).to.eq("MultipleOneOfError");
         });
--- test/integration/validate.JsonEditor.test.ts
+++ test/integration/validate.JsonEditor.test.ts
@@ -1,8 +1,10 @@
 import path from "path";
 import fs from "fs";
 import { expect } from "chai";
+import { strict as assert } from "assert";
 import { JsonEditor as Core } from "../../lib/jsoneditor";
-import { JsonSchema } from "../../lib/types";
+import { JsonSchema, isJsonError } from "../../lib/types";
+import { isSchemaNode } from "../../lib/schemaNode";
 
 describe("validate.JsonEditor", () => {
     let schema: JsonSchema;
@@ -20,7 +22,7 @@ describe("validate.JsonEditor", () => {
     });
 
     it("should validate test-data by default", () => {
-        const errors = draft.validate(schema, data);
+        const errors = draft.validate(draft.createNode(schema), data);
 
         expect(errors).to.have.length(0);
     });
@@ -28,62 +30,87 @@ describe("validate.JsonEditor", () => {
     describe("resolveRef", () => {
         it("should merge any properties where $ref is used", () => {
             draft.rootSchema = { definitions: { def: { type: "string" } } };
-            const result = draft.resolveRef({ $ref: "#/definitions/def", title: "a definition" });
+            const node = draft.resolveRef(
+                draft.createNode({ $ref: "#/definitions/def", title: "a definition" })
+            );
+
+            assert(isSchemaNode(node));
+            const result = node.schema;
             expect(result).to.deep.include({ type: "string", title: "a definition" });
         });
     });
 
     describe("oneOf", () => {
         it("should return a matching schema", () => {
-            const result = draft.resolveOneOf(5, {
-                oneOf: [{ type: "string" }, { type: "number" }]
-            });
-            expect(result).to.deep.equal({ type: "number" });
+            const node = draft.resolveOneOf(
+                draft.createNode({
+                    oneOf: [{ type: "string" }, { type: "number" }]
+                }),
+                5
+            );
+
+            assert(isSchemaNode(node));
+            const result = node.schema;
+            expect(result).to.deep.equal({ __oneOfIndex: 1, type: "number" });
         });
 
         it("should return an error if a matching schema could not be found", () => {
-            const result = draft.resolveOneOf([], {
-                oneOf: [{ type: "string" }, { type: "number" }]
-            });
+            const result = draft.resolveOneOf(
+                draft.createNode({
+                    oneOf: [{ type: "string" }, { type: "number" }]
+                }),
+                []
+            );
+
+            assert(isJsonError(result));
             expect(result.type).to.eq("error");
             expect(result.name).to.eq("OneOfError");
         });
 
         it("should return an error if multiple schemas match the data", () => {
-            const result = draft.resolveOneOf(4, {
-                oneOf: [
-                    { type: "string" },
-                    { type: "number", minimum: 2 },
-                    { type: "number", minimum: 3 }
-                ]
-            });
+            const result = draft.resolveOneOf(
+                draft.createNode({
+                    oneOf: [
+                        { type: "string" },
+                        { type: "number", minimum: 2 },
+                        { type: "number", minimum: 3 }
+                    ]
+                }),
+                4
+            );
 
+            assert(isJsonError(result));
             expect(result.type).to.eq("error");
             expect(result.name).to.eq("MultipleOneOfError");
         });
 
         describe("oneOfProperty", () => {
             it("should return schema where 'oneOfProperty'-value matches schema", () => {
-                const result = draft.resolveOneOf(
-                    { id: "2" },
-                    {
+                const node = draft.resolveOneOf(
+                    draft.createNode({
                         oneOfProperty: "id",
                         oneOf: [
                             { properties: { id: { pattern: "^1$" } } },
                             { properties: { id: { pattern: "^2$" } } },
                             { properties: { id: { pattern: "^3$" } } }
                         ]
-                    }
+                    }),
+                    { id: "2" }
                 );
-                expect(result).to.deep.equal({ properties: { id: { pattern: "^2$" } } });
+
+                assert(isSchemaNode(node));
+                const result = node.schema;
+                expect(result).to.deep.equal({
+                    __oneOfIndex: 1,
+                    properties: { id: { pattern: "^2$" } }
+                });
             });
         });
 
         describe("fuzzyMatch", () => {
             it("should return schema where most properties match", () => {
-                const result = draft.resolveOneOf(
-                    { id: "4", a: 1, b: 2 },
-                    {
+                const node = draft.resolveOneOf(
+                    draft.createNode({
                         oneOf: [
                             { properties: { id: { pattern: "^1$" }, a: { type: "object" } } },
                             {
@@ -95,9 +122,14 @@ describe("validate.JsonEditor", () => {
                             },
                             { properties: { id: { pattern: "^3$" }, a: { type: "number" } } }
                         ]
-                    }
+                    }),
+                    { id: "4", a: 1, b: 2 }
                 );
+
+                assert(isSchemaNode(node));
+                const result = node.schema;
                 expect(result).to.deep.equal({
+                    __oneOfIndex: 1,
                     properties: {
                         id: { pattern: "^2$" },
                         a: { type: "string" },
