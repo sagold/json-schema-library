e66e575705d7cef38a63d397a57a73eebb6c3a88
Sascha Goldhofer <github@saschagoldhofer.de>
2025-03-29 00:09:47 +0100

    chore: remove more of v1

--- _v1/utils/uniqueItems.ts
+++
@@ -1,6 +0,0 @@
-/**
- * @returns list with unique values only
- */
-export function uniqueItems(list: (number | string | boolean | null | undefined)[]) {
-    return list.filter((item, index) => list.indexOf(item) === index);
-}
