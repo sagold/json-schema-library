33da797619e264c8f28d4229acce699364865d71
Sascha Goldhofer <github@saschagoldhofer.de>
2025-03-28 23:45:12 +0100

    add: extend draft helper locally to draftEditor

--- src/draftEditor.ts
+++ src/draftEditor.ts
@@ -1,42 +1,40 @@
 import { draft2019 } from "./draft2019";
 import { oneOfFuzzyFeature } from "./features/oneOf";
-import { Draft } from "./types";
+import { Draft, Feature } from "./types";
+
+function addFeature(draft: Draft, feature: Feature): Draft {
+    const features = [...draft.features];
+    const index = features.findIndex((f) => f.keyword === feature.keyword);
+    if (index === -1) {
+        features.push(feature);
+    } else {
+        features[index] = feature;
+    }
+    return {
+        ...draft,
+        features
+    };
+}
+
+function pimpFeatures(draft: Draft) {
+    draft.features.forEach((feature) => {
+        const logKeyword = () => feature.keyword;
+        if (feature.validate) {
+            feature.validate.toJSON = logKeyword;
+        }
+        if (feature.reduce) {
+            feature.reduce.toJSON = logKeyword;
+        }
+        if (feature.resolve) {
+            feature.resolve.toJSON = logKeyword;
+        }
+    });
+    return draft;
+}
 
 /**
- * @draft-2019 https://json-schema.org/draft/2019-09/release-notes
+ * @draft-editor https://json-schema.org/draft/2019-09/release-notes
  *
- * new
- * - $anchor
- * - $recursiveAnchor and $recursiveRef
- * - $vocabulary
- *
- * changed
- * - $defs (renamed from definitions)
- * - $id
- * - $ref
- * - dependencies has been split into dependentSchemas and dependentRequired
+ * Uses Draft 2019-09 and changes resolveOneOf to be fuzzy
  */
-export const draftEditor: Draft = {
-    version: "draft-2019-09",
-    $schema: "https://json-schema.org/draft/2019-09/schema",
-    features: draft2019.features
-        .map((feature) => {
-            if (feature.keyword === "oneOf") {
-                return oneOfFuzzyFeature;
-            }
-            return feature;
-        })
-        .map((feature) => {
-            const logKeyword = () => feature.keyword;
-            if (feature.validate) {
-                feature.validate.toJSON = logKeyword;
-            }
-            if (feature.reduce) {
-                feature.reduce.toJSON = logKeyword;
-            }
-            if (feature.resolve) {
-                feature.resolve.toJSON = logKeyword;
-            }
-            return feature;
-        })
-};
+export const draftEditor: Draft = pimpFeatures(addFeature(draft2019, oneOfFuzzyFeature));
