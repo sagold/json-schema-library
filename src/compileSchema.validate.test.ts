0f1b8db72dc6e0fa2a6d266fc97955d95537ecf4
Sascha Goldhofer <github@saschagoldhofer.de>
2025-04-07 23:27:14 +0200

    ci: fix unit test export

--- src/compileSchema.validate.test.ts
+++ src/compileSchema.validate.test.ts
@@ -10,7 +10,6 @@ describe("compileSchema.validate", () => {
                 const { errors } = compileSchema({
                     exclusiveMaximum: 0
                 }).validate(0);
-                console.log("errors", errors);
                 assert.deepEqual(errors.length, 1);
             });
 
