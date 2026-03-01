8fe8637b81b985ebb6e42fd8be7efcad9de58d2f
Sascha Goldhofer <github@saschagoldhofer.de>
2026-03-01 14:07:33 +0100

    chore: cleanup tests and update to latest changed on getNodeChild

--- src/tests/issues/issue93.booleanSchema.test.ts
+++ src/tests/issues/issue93.booleanSchema.test.ts
@@ -1,58 +1,64 @@
 import { strict as assert } from "assert";
 import { compileSchema } from "../../compileSchema";
-import { draft07 } from "../../draft07";
 
 describe("issue#93 - boolean schema typescript support", () => {
     it("compileSchema should accept false as boolean schema", () => {
-        const rootNode = compileSchema({ $schema: "draft-07", properties: { test: false } }, { drafts: [draft07] });
-        const { node } = rootNode.getNode("/test", { name: "hello" });
-        assert(node !== undefined);
+        const rootNode = compileSchema({ $schema: "draft-2020-12", properties: { test: false } });
+        const { node } = rootNode.getNode("test", { name: "hello" });
+        assert(node);
         assert.equal(node.schema, false);
     });
 
     it("compileSchema should accept true as boolean schema", () => {
-        const rootNode = compileSchema({ $schema: "draft-07", properties: { test: true } }, { drafts: [draft07] });
-        const { node } = rootNode.getNode("/test", { name: "hello" });
-        assert(node !== undefined);
-        assert.equal(node.schema, true);
+        const rootNode = compileSchema({ $schema: "draft-2020-12", properties: { test: true } });
+        const nodeChildResult = rootNode.getNodeChild("test", { name: "hello" });
+        assert(nodeChildResult.node);
+        assert.equal(nodeChildResult.node.schema, true);
+
+        const { node } = rootNode.getNode("test", { name: "hello" });
+        assert(node);
+        assert.deepEqual(node.schema, {}, "reduced boolean schema `true` should have been converted to schema-object");
     });
 
     it("getNode should work with boolean schema false", () => {
-        const rootNode = compileSchema({ $schema: "draft-07", properties: { test: false } }, { drafts: [draft07] });
+        const rootNode = compileSchema({ $schema: "draft-2020-12", properties: { test: false } });
         const { node } = rootNode.getNode("/test", {});
-        assert(node !== undefined);
+        assert(node);
         assert.equal(node.schema, false);
     });
 
     it("getData should work with boolean schema", () => {
-        const rootNode = compileSchema({ $schema: "draft-07", properties: { name: { type: "string" }, test: false } }, { drafts: [draft07] });
+        const rootNode = compileSchema({
+            $schema: "draft-2020-12",
+            properties: { name: { type: "string" }, test: false }
+        });
         const data = rootNode.getData();
-        assert(data !== undefined);
+        assert(data);
     });
 
     it("addRemoteSchema should accept boolean schema", () => {
-        const rootNode = compileSchema({ $schema: "draft-07" }, { drafts: [draft07] });
+        const rootNode = compileSchema({ $schema: "draft-2020-12" });
         rootNode.addRemoteSchema("http://example.com/bool", false);
         assert(true);
     });
 
     it("getNodeRoot should work when schema contains boolean schemas", () => {
-        const rootNode = compileSchema({ $schema: "draft-07", properties: { test: false } }, { drafts: [draft07] });
+        const rootNode = compileSchema({ $schema: "draft-2020-12", properties: { test: false } });
         const { node } = rootNode.getNode("/test", {});
-        assert(node !== undefined);
+        assert(node);
         const root = node.getNodeRoot();
-        assert(root !== undefined);
+        assert(root);
     });
 
     it("compileSchema should accept false as root boolean schema", () => {
         const rootNode = compileSchema(false);
-        assert(rootNode !== undefined);
+        assert(rootNode);
         assert.equal(rootNode.schema, false);
     });
 
     it("compileSchema should accept true as root boolean schema", () => {
         const rootNode = compileSchema(true);
-        assert(rootNode !== undefined);
+        assert(rootNode);
         assert.equal(rootNode.schema, true);
     });
     it("getNode should work on compileSchema(false)", () => {
